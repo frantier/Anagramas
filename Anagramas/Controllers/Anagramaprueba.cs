6ab701178f8faed85dce6d541018beac1bd99ac5
20090634@alumno.utmetropolitana.edu.mx <20090634@alumno.utmetropolitana.edu.mx>
2021-10-04 11:38:39 -0500

    Agregar archivos de proyecto.

---
+++ Anagramas/Controllers/Anagramaprueba.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+/*Nombre de la escuela: Universidad Tecnologica Metropolitana
+Asignatura: Aplicaciones Web Orientadas a Objetos
+Nombre del Maestro: Chuc Uc Joel Ivan
+Nombre de la actividad: Palindromos
+Nombre del alumno: Pedro Victor Ruvalcaba Novelo
+Cuatrimestre: 4
+Grupo: B
+Parcial: 1
+*/
+
+namespace Anagramas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Anagramaprueba : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Anagrama(string ana1, string ana2)
+        {
+            bool anagramas;
+            var RoT = "";
+            if (ana1.Length != ana2.Length)
+            {
+                anagramas = false;
+            }
+            else
+            {
+                char[] l1 = ana1.ToCharArray();
+                Array.Sort(l1);
+                char[] l2 = ana2.ToCharArray();
+                Array.Sort(l2);
+
+
+
+                string pal = new string(l1);
+                string pal2 = new string(l2);
+                if (pal == pal2)
+                {
+                    anagramas = true;
+                }
+                else
+                {
+                    anagramas = false;
+                }
+            }
+            if (anagramas == true)
+            {
+                RoT = "Las palabras " + ana1 + " y " + ana2 + " Son Anagramas";
+            }
+            else
+            {
+                RoT = "Las palabras " + ana1 + " y " + ana2 + " No son Anagramas";
+            }
+            return Ok(RoT);
+        }
+    }
+}
